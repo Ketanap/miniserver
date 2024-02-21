1a938e38c02ccb73737e778f795e49dd8f83b508
KetanPatelHTT <ketanpatelhtt22@gmail.com>
2024-02-21 12:05:04 +0530

    cors update

--- api/server.js
+++ api/server.js
@@ -26,11 +26,15 @@ const rules = auth.rewriter({
 server.use(rules)
 //server.use(middlewares)
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'authorization');
-    res.header('Access-Control-Request-Headers', 'authorization');
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Request-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     if (req.path == '/api/auth/login') {
       next();
     } else {
